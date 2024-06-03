238d12bc0e28b8465a9b43e683d043a3d5a5a789
JoahanOlvera <joahanolveraxd@gmail.com>
2024-06-03 13:23:49 -0700

    Mezcla entre front y back end, actualizacion de vistas

--- src/pages/obtenerActivos.vue
+++ src/pages/obtenerActivos.vue
@@ -1,5 +1,5 @@
 <template>
-   <v-card flat title="Datos de los activos">
+  <v-card flat title="Datos de los activos">
     <template v-slot:text>
       <v-text-field
         v-model="search"
@@ -11,7 +11,11 @@
       ></v-text-field>
     </template>
 
-    <v-data-table :headers="headers" :items="dataItems" :search="search"></v-data-table>
+    <v-data-table :headers="headers" :items="dataItems" :search="search">
+      <template v-slot:item.imagenBase64="{ item }">
+        <v-img :src="'data:image/png;base64,' + item.imagenBase64" height="100px"></v-img>
+      </template>
+    </v-data-table>
   </v-card>
 
   <v-container class="d-flex align-center justify-center">
@@ -29,9 +33,15 @@
         <v-text-field v-model="nuevoActivo.numeroInventario" label="Numero de inventario"></v-text-field>
         <v-text-field v-model="nuevoActivo.tipo" label="Tipo"></v-text-field>
         <v-text-field v-model="nuevoActivo.descripcion" label="Descripcion"></v-text-field>
-        <v-text-field v-model="nuevoActivo.ubicacionId" label="Id de ubicación"></v-text-field>
-        <v-text-field v-model="nuevoActivo.responsableId" label="Id de responsable"></v-text-field>
-        <v-text-field v-model="nuevoActivo.imagen" label="Imagen"></v-text-field>
+        <v-text-field v-model="nuevoActivo.ubicacionId" label="Id de ubicación" type="number"></v-text-field>
+        <v-text-field v-model="nuevoActivo.responsableId" label="Id de responsable" type="number"></v-text-field>
+        <v-file-input
+          v-model="nuevoActivo.imagen"
+          label="Imagen"
+          accept="image/*"
+          prepend-icon="mdi-camera"
+          @change="handleImageUpload"
+        ></v-file-input>
       </v-card-text>
       <v-card-actions>
         <v-btn color="primary" @click="agregarActivo">Agregar</v-btn>
@@ -62,167 +72,184 @@
         <v-text-field v-model="activoAActualizar.numeroInventario" label="Numero de inventario"></v-text-field>
         <v-text-field v-model="activoAActualizar.tipo" label="Tipo"></v-text-field>
         <v-text-field v-model="activoAActualizar.descripcion" label="Descripcion"></v-text-field>
-        <v-text-field v-model="activoAActualizar.ubicacionId" label="Id de ubicación"></v-text-field>
-        <v-text-field v-model="activoAActualizar.responsableId" label="Id de responsable"></v-text-field>
-        <v-text-field v-model="activoAActualizar.imagen" label="Imagen"></v-text-field>
+        <v-text-field v-model="activoAActualizar.ubicacionId" label="Id de ubicación" type="number"></v-text-field>
+        <v-text-field v-model="activoAActualizar.responsableId" label="Id de responsable" type="number"></v-text-field>
+        <v-file-input
+          v-model="activoAActualizar.imagen"
+          label="Imagen"
+          accept="image/*"
+          prepend-icon="mdi-camera"
+          @change="handleImageUpload"
+        ></v-file-input>
       </v-card-text>
       <v-card-actions>
         <v-btn color="primary" @click="actualizarActivo(activoAActualizar.id)">Actualizar</v-btn>
         <v-btn color="error" @click="cerrarDialogoActualizarActivo">Cancelar</v-btn>
       </v-card-actions>
     </v-card>
-  </v-dialog> 
-  </template>
-  
-  <script>
-  
-  export default {
-    name: 'obtenerActivos',
-    data() {
-      return {
-        search: '',
-        dataItems: [],
-        headers: [
-          { text: 'ID', value: 'id'},
-          { text: 'Numero de serie', value: 'numeroSerie'},
-          { text: 'Numero de inventario', value: 'numeroInventario'},
-          { text: 'Tipo', value: 'tipo'},
-          { text: 'Descripcion', value: 'descripcion'},
-          { text: 'Id de ubicacion', value: 'ubicacionId'},
-          { text: 'Id de responsable', value: 'responsableId'},
-          { text: 'Imagen', value: 'imagen'},
-        ],
-        dialogAgregarActivo: false,
-        dialogBorrarActivo: false,
-        dialogActualizarActivo: false,
-        nuevoActivo: {
-          id: '',
-          numeroSerie: '',
-          numeroInventario: '',
-          tipo: '',
-          descripcion: '',
-          ubicacionId: '',
-          responsableId: '',
-          imagen: ''
-        },
-        activoABorrar: {
-          id: ''
-        },
-        activoAActualizar: {
-          id: '',
-          numeroSerie: '',
-          numeroInventario: '',
-          tipo: '',
-          descripcion: '',
-          ubicacionId: '',
-          responsableId: '',
-          imagen: ''
-        }
-      };
-    },
-    mounted() {
-      this.getActivos();
-    },
-    methods: {
-  async getActivos() {
-  try {
-    const response = await fetch('https://localhost:4000/activos', {
-            method: 'GET',
-            mode: 'cors',
-            credentials: 'same-origin', // o 'include' si estás utilizando cookies
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'obtenerActivos',
+  data() {
+    return {
+      search: '',
+      dataItems: [],
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Numero de serie', value: 'numeroSerie' },
+        { text: 'Numero de inventario', value: 'numeroInventario' },
+        { text: 'Tipo', value: 'tipo' },
+        { text: 'Descripcion', value: 'descripcion' },
+        { text: 'Id de ubicacion', value: 'ubicacionId' },
+        { text: 'Id de responsable', value: 'responsableId' },
+        { text: 'Imagen', value: 'imagenBase64' },
+      ],
+      dialogAgregarActivo: false,
+      dialogBorrarActivo: false,
+      dialogActualizarActivo: false,
+      nuevoActivo: {
+        id: '',
+        numeroSerie: '',
+        numeroInventario: '',
+        tipo: '',
+        descripcion: '',
+        ubicacionId: '',
+        responsableId: '',
+        imagenBase64: ''
+      },
+      activoABorrar: {
+        id: ''
+      },
+      activoAActualizar: {
+        id: '',
+        numeroSerie: '',
+        numeroInventario: '',
+        tipo: '',
+        descripcion: '',
+        ubicacionId: '',
+        responsableId: '',
+        imagenBase64: ''
+      }
+    };
+  },
+  mounted() {
+    this.getActivos();
+  },
+  methods: {
+    async getActivos() {
+      try {
+        const response = await fetch('https://localhost:4000/activos', {
+          method: 'GET',
+          mode: 'cors',
+          credentials: 'same-origin', // o 'include' si estás utilizando cookies
         });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    this.dataItems = data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-},
-  async eliminarActivo(id) {
-    try {
-      const response = await fetch(`https://localhost:4000/activos/eliminarPorId/${id}`, {
-        method: 'DELETE',
-        mode: 'cors',
-        credentials: 'same-origin',
-      });
-      this.getActivos();
-      if(!response.ok){
-        throw new Error('Error al borrar el activo');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        this.dataItems = data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  },
-  async agregarActivo() {
-    try {
-      const response = await fetch(`https://localhost:4000/activos`, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-        'Content-Type': 'application/json' // Indica que estás enviando datos en formato JSON
-        },
-        body: JSON.stringify({
-          id: this.nuevoActivo.id,
-          numeroSerie: this.nuevoActivo.numeroSerie,
-          numeroInventario: this.nuevoActivo.numeroInventario,
-          tipo: this.nuevoActivo.tipo,
-          descripcion: this.nuevoActivo.descripcion,
-          ubicacionId: this.nuevoActivo.ubicacionId,
-          responsableId: this.nuevoActivo.responsableId,
-          imagen: this.nuevoActivo.imagen
-        }),
-        credentials: 'same-origin',
-      });
-      this.getActivos();
-      if(response.status==400){
-        throw new Error('Error al agregar el activo, ese ID ya existe');
+    },
+    async eliminarActivo(id) {
+      try {
+        const response = await fetch(`https://localhost:4000/activos/eliminarPorId/${id}`, {
+          method: 'DELETE',
+          mode: 'cors',
+          credentials: 'same-origin',
+        });
+        this.getActivos();
+        if (!response.ok) {
+          throw new Error('Error al borrar el activo');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      console.log(response.status);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  },
-  async actualizarActivo(id) {
-    try {
-      const existe = await fetch(`https://localhost:4000/activos/buscarPorId/${id}`, {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'same-origin'
-      });
+    },
+    async agregarActivo() {
+      try {
+        const response = await fetch(`https://localhost:4000/activos`, {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json' // Indica que estás enviando datos en formato JSON
+          },
+          body: JSON.stringify({
+            id: this.nuevoActivo.id,
+            numeroSerie: this.nuevoActivo.numeroSerie,
+            numeroInventario: this.nuevoActivo.numeroInventario,
+            tipo: this.nuevoActivo.tipo,
+            descripcion: this.nuevoActivo.descripcion,
+            ubicacionId: this.nuevoActivo.ubicacionId || null,
+            responsableId: this.nuevoActivo.responsableId || null,
+            imagenBase64: this.nuevoActivo.imagenBase64
+          }),
+          credentials: 'same-origin',
+        });
+        if (!response.ok) {
+          throw new Error('Error al agregar el activo');
+        }
+        this.getActivos();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    async actualizarActivo(id) {
+      try {
+        const existe = await fetch(`https://localhost:4000/activos/buscarPorId/${id}`, {
+          method: 'GET',
+          mode: 'cors',
+          credentials: 'same-origin'
+        });
 
-      if(existe.status==404){
-        throw new Error('Error al verificar la existencia del activo');
-      }
+        if (existe.status == 404) {
+          throw new Error('Error al verificar la existencia del activo');
+        }
 
-      const response = await fetch(`https://localhost:4000/activos/actualizarPorId/${id}`, {
-        method: 'PUT',
-        mode: 'cors',
-        headers: {
-        'Content-Type': 'application/json' // Indica que estás enviando datos en formato JSON
-        },
-        body: JSON.stringify({
-          numeroSerie: this.activoAActualizar.numeroSerie,
-          numeroInventario: this.activoAActualizar.numeroInventario,
-          tipo: this.activoAActualizar.tipo,
-          descripcion: this.activoAActualizar.descripcion,
-          ubicacionId: this.activoAActualizar.ubicacionId,
-          responsableId: this.activoAActualizar.responsableId,
-          imagen: this.activoAActualizar.imagen
-        }),
-        credentials: 'same-origin',
-      });
-      console.log(response.body);
-      this.getActivos();
-      console.log(response.status);
-      if(response.status==404){
-        throw new Error('Error al actualizar el activo, ese ID no existe');
+        const response = await fetch(`https://localhost:4000/activos/actualizarPorId/${id}`, {
+          method: 'PUT',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json' // Indica que estás enviando datos en formato JSON
+          },
+          body: JSON.stringify({
+            numeroSerie: this.activoAActualizar.numeroSerie,
+            numeroInventario: this.activoAActualizar.numeroInventario,
+            tipo: this.activoAActualizar.tipo,
+            descripcion: this.activoAActualizar.descripcion,
+            ubicacionId: this.activoAActualizar.ubicacionId || null,
+            responsableId: this.activoAActualizar.responsableId || null,
+            imagenBase64: this.activoAActualizar.imagenBase64
+          }),
+          credentials: 'same-origin',
+        });
+        if (!response.ok) {
+          throw new Error('Error al actualizar el activo');
+        }
+        this.getActivos();
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  },
+    },
+    handleImageUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const base64 = e.target.result.split(',')[1];
+          if (this.dialogAgregarActivo) {
+            this.nuevoActivo.imagenBase64 = base64;
+          } else if (this.dialogActualizarActivo) {
+            this.activoAActualizar.imagenBase64 = base64;
+          }
+        };
+        reader.readAsDataURL(file);
+      }
+    },
     abrirDialogoAgregarActivo() {
       this.dialogAgregarActivo = true;
     },
@@ -241,6 +268,6 @@
     cerrarDialogoActualizarActivo() {
       this.dialogActualizarActivo = false;
     }
-},
-  };
-  </script>
+  },
+};
+</script>
